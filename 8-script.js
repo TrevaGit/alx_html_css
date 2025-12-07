fdc9d6e89591c9096f026e6d3c851a3e963c7c53
Treva Onyango Aoko <trevaaoko1235@gmail.com>
2025-12-07 15:53:10 +0300

    Create 8-script.js

---
+++ 8-script.js
@@ -0,0 +1,8 @@
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("show");
+});
